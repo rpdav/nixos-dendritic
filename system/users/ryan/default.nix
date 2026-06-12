3b4b3bf069ed9684c70e5689014701b25eb4ac37
ryan <105075689+rpdav@users.noreply.github.com>
2026-06-12 15:29:36 -0400

    standalone flake-parts HM config
    this works with home-manager switch --flake .#ryanDendritic

--- system/users/ryan/default.nix
+++ system/users/ryan/default.nix
@@ -15,29 +15,42 @@
       extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
       openssh.authorizedKeys.keys = ["sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILygGVzteEOsvhdTTP+guA4Fq0TeJM/R2tDYXXbHvhLFAAAABHNzaDo= ryan@yubinano"];
     };
-    home-manager = {
-      #useUserPackages = true;
-      users.ryan = {
+
+    environment.systemPackages = with pkgs; [home-manager];
+
+    #home-manager = {
+    #  #useUserPackages = true;
+    #  users.ryan = {
+    #    home.username = "ryan";
+    #    home.homeDirectory = "/home/ryan";
+    #    home.stateVersion = "26.05";
+    #    home.packages = with pkgs; [cowsay];
+    #  };
+
+    #  ## From
+    #  #users.ryan = import (configLib.relativeToRoot "home/ryan/${config.networking.hostName}.nix");
+    #  #extraSpecialArgs = {
+    #  #  inherit nixpkgs-stable;
+    #  #  inherit secrets;
+    #  #  inherit inputs;
+    #  #  inherit outputs;
+    #  #  inherit configLib;
+    #  #};
+    #};
+  };
+  flake.homeConfigurations.ryanDendritic = inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+    modules = [
+      ({pkgs, ...}: {
         home.username = "ryan";
         home.homeDirectory = "/home/ryan";
-        home.stateVersion = "26.05";
-        home.packages = with pkgs; [cowsay];
-      };
-      #users.ryan = import (configLib.relativeToRoot "home/ryan/${config.networking.hostName}.nix");
-      #extraSpecialArgs = {
-      #  inherit nixpkgs-stable;
-      #  inherit secrets;
-      #  inherit inputs;
-      #  inherit outputs;
-      #  inherit configLib;
-      #};
-    };
-  };
-  #flake.homeConfigurations.ryanDendritic = {pkgs, ...}: {
-  #  home.username = "ryan";
-  #  home.homeDirectory = "/home/ryan";
 
-  #  home.stateVersion = "26.05"; # don't change without reading release notes
-  #  home.packages = with pkgs; [cowsay];
-  #};
+        home.stateVersion = "26.05"; # don't change without reading release notes
+        home.packages = with pkgs; [
+          cowsay
+          lolcat
+        ];
+      })
+    ];
+  };
 }
